2a7d1eaee4d40e1d66ec8502ef0f003423348d6e
vanntile <33692650+vanntile@users.noreply.github.com>
2023-10-24 09:03:38 +0300

    feat: :star2: add limit query parameter to server reading route

--- server/main.py
+++ server/main.py
@@ -1,6 +1,7 @@
 import uuid
+from typing import Annotated
 
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, status, HTTPException, Query
 from pydantic import UUID4
 
 from models import Route
@@ -11,7 +12,10 @@ routes: list[Route] = []
 
 
 @app.get("/routes", response_model=list[Route], status_code=status.HTTP_200_OK)
-def read_routes():
+def read_routes(limit: Annotated[int | None, Query(ge=0)] = None):
+    print(limit)
+    if limit is not None:
+        return routes[:limit]
     return routes
 
 
